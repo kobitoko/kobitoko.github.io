2b0368863e4da7f3851797de312d6d7cccc15be6
Ryan <kobitoko@gmail.com>
2017-10-14 01:12:31 -0600

    just messed up the range of splitting, githubpages changes \r\n to \n

--- js/portfolio.js
+++ js/portfolio.js
@@ -26,7 +26,7 @@ function init() {
 }
 
 function getArticles() {
-    $.getJSON("https://raw.githubusercontent.com/kobitoko/kobitoko.github.io/master/articles.json", {}, crawlArticles);
+    $.getJSON("articles.json", {}, crawlArticles);
 }
 
 var articleTags = {}, 
@@ -43,7 +43,6 @@ function downloadArticles() {
     var promises = [];
     $.each(articleTags, function(key) {
         promises.push(
-		console.log("articleAjax "+key);
         $.ajax({
             mimeType: 'text/plain; charset=x-user-defined',
             url: "https://raw.githubusercontent.com/kobitoko/kobitoko.github.io/master/articles/"+key+".md",
@@ -51,7 +50,6 @@ function downloadArticles() {
             dataType: "text",
             cache: false,
             success: function(data) {
-				console.log("ayy "+data);
                 articles[key] = String(data);
                 Promise.resolve();
             }
@@ -66,7 +64,7 @@ function createGrid() {
     $.each(articleTags, function(key, val) {
         var title = key.replace("_"," "),
             // Display the image and the first paragraph.(double newline)
-            shortBlurb = articles[key].split('\r\n\r\n'),
+            shortBlurb = articles[key].split('\n\n'),
             content = String(shortBlurb[1]+"<br>"+shortBlurb[2]);
         $(".grid")[0].innerHTML += "<div class='grid-item'><img src='https://raw.githubusercontent.com/kobitoko/kobitoko.github.io/master/articles/"+key+".jpg'><article><h3>"+title+"</h3><br>"+ converter.makeHtml(content) +"</article>";
     });
